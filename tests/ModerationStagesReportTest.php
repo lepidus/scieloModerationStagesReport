ef0e9a7df47e0f3d4202519a762b95d7511c9234
Jhon <jhon@lepidus.com.br>
2022-06-20 16:09:27 -0400

    Adds tests for the Moderation Stages Report
    Issue: documentacao-e-tarefas/scielo#348
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>
    Signed-off-by: Yves <yves@lepidus.com.br>

---
+++ tests/ModerationStagesReportTest.php
@@ -0,0 +1,61 @@
+<?php
+use PHPUnit\Framework\TestCase;
+import ('plugins.reports.scieloModerationStagesReport.classes.ModerationStagesReport');
+
+class ModerationStagesReportTest extends TestCase {
+    
+    private $report;
+    private $filePath = "/tmp/test.csv";
+
+    public function setUp() : void {
+        $submissions = [
+            1 => __("plugins.generic.scieloModerationStages.stages.formatStage"),
+            2 => __("plugins.generic.scieloModerationStages.stages.contentStage")
+        ];
+        $this->report = new ModerationStagesReport($submissions);
+    }
+
+    public function tearDown() : void {
+        if (file_exists(($this->filePath))) 
+            unlink($this->filePath);
+    }
+
+    protected function createCSVReport() : void {
+        $csvFile = fopen($this->filePath, 'wt');
+        $this->report->buildCSV($csvFile);
+        fclose($csvFile);
+    }
+
+    public function testGeneratedReportHasHeaders(): void {
+        $this->createCSVReport();
+        $csvRows = array_map('str_getcsv', file($this->filePath));
+
+        $expectedHeaders = [
+            __("plugins.reports.scieloModerationStagesReport.headers.submissionId"),
+            __("plugins.reports.scieloModerationStagesReport.headers.moderationStage")
+        ];
+        $firstRow = $csvRows[0];
+
+        $this->assertEquals($expectedHeaders, $firstRow);
+    }
+    
+    public function testGeneratedReportHasSubmissions(): void {
+        $this->createCSVReport();
+        $csvRows = array_map('str_getcsv', file($this->filePath));
+
+        $secondRow = $csvRows[1];
+        $expectedSubmissionRow = ["1", __("plugins.generic.scieloModerationStages.stages.formatStage")];
+
+        $this->assertEquals($expectedSubmissionRow, $secondRow);
+    }
+
+    public function testGeneratedReportHasUTF8Bytes(): void {
+        $this->createCSVReport();
+
+        $expectedBOM = chr(0xEF).chr(0xBB).chr(0xBF);
+        $BOMRead = fread($this->csvFile, strlen($expectedBOM));
+
+        $this->assertEquals($expectedBOM, $BOMRead);
+    }
+
+}
